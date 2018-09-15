2a0b4cb55d6585211b3cf6f6bf699748fcc265ac
Delphingg <43294512+Delphingg@users.noreply.github.com>
2018-09-15 15:00:05 +0300

    Update discord_bitcoin_price_bot.py

--- discord_bitcoin_price_bot.py
+++ discord_bitcoin_price_bot.py
@@ -6,9 +6,31 @@ from discord import Game
 from discord.ext.commands import Bot
 
 BOT_PREFIX = ("?", "!")
+TOKEN = "NDg1NTAzMzQ5NTE3NzEzNDE4.Dn6Fjg.OnCymgrFXIFAvOLMJKQBPfNCDzY"  # Get at discordapp.com/developers/applications/me
 
 client = Bot(command_prefix=BOT_PREFIX)
-bot = commands.Bot(command_prefix='?', '!')
+
+@client.command(name='8ball',
+                description="Answers a yes/no question.",
+                brief="Answers from the beyond.",
+                aliases=['eight_ball', 'eightball', '8-ball'],
+                pass_context=True)
+async def eight_ball(context):
+    possible_responses = [
+        'That is a resounding no',
+        'It is not looking likely',
+        'Too hard to tell',
+        'It is quite possible',
+        'Definitely',
+    ]
+    await client.say(random.choice(possible_responses) + ", " + context.message.author.mention)
+
+
+@client.command()
+async def square(number):
+    squared_value = int(number) * int(number)
+    await client.say(str(number) + " squared is " + str(squared_value))
+
 
 @client.command()
 async def bitcoin():
@@ -18,30 +40,14 @@ async def bitcoin():
         response = await raw_response.text()
         response = json.loads(response)
         await client.say("Bitcoin price is: $" + response['bpi']['USD']['rate'])
-        
-@client.command(name='Шар',
-                description="Answers a yes/no question.",
-                brief="Answers from the beyond.",
-                aliases=['Шарик', '8Шарик', '8Шар', 'шар', '8шарик', '8шар', 'ШАРИК', '8ШАРИК', '8ШАР', 'ШАР'],
-                pass_context=True)
-async def eight_ball(context):
-    possible_responses = [
-        'Определённо нет',
-        'Вероятно, нет',
-        'Сложно сказать',
-        'Возможно',
-        'Определённо да',
-    ]
-    await client.say(random.choice(possible_responses) + ", " + context.message.author.mention)
-    
-    
-    
-@client.command()
-async def square(number):
-    squared_value = int(number) * int(number)
-    await client.say(str(number) + " squared is " + str(squared_value))
 
 
+async def list_servers():
+    await client.wait_until_ready()
+    while not client.is_closed:
+        print("Current servers:")
+        for server in client.servers:
+            print(server.name)
+        await asyncio.sleep(600)
 
-client.run("NDg1NTAzMzQ5NTE3NzEzNDE4.Dm0jJw.RjBFbnkIQEcPE-zpP6EHTyquzoQ")
-
+client.run('NDg1NTAzMzQ5NTE3NzEzNDE4.Dn6Fjg.OnCymgrFXIFAvOLMJKQBPfNCDzY')        
